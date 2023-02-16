e9c97fda50a173e362ef186ba9475dcee33f90a6
Matthew Templeton <matthew.templeton@cfa.harvard.edu>
2023-02-16 11:44:36 -0500

    modified:   adsdocmatch/match_w_metadata.py  	modified:   config.py

--- adsdocmatch/match_w_metadata.py
+++ adsdocmatch/match_w_metadata.py
@@ -309,9 +309,9 @@ class MatchMetadata():
                         elif (len(combined_result) >= 8 and float(combined_result[7]) >= 0.5 and
                                   (('None' in combined_result[8]) or ('Multi match' in combined_result[5]))):
                             combined_result[2] = 'verify'
-                        fp.write(','.join(combined_result)+'\n')
                     except:
                         pass
+                    fp.write(','.join(combined_result)+'\n')
 
     def merge_classic_docmatch_results(self, classic_filename, docmatch_filename, output_filename):
         """
